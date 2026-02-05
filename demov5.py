563860d8e358dde29eeb199ff7e8ba521aafa4c7
YagoSanabria <yago.sanabria.gavin@gmail.com>
2026-02-05 13:57:12 +0100

    comienzo publicacion en pages

--- demov5.py
+++ demov5.py
@@ -137,4 +137,4 @@ imprimir_arbol_con_pesos(G, pr_v3, "VERSIÓN 3: Con Referencias y Pesos (refs=3x
 # TABLA COMPARATIVA FINAL
 tabla_comparativa_final(G, pr_v1, pr_v2, pr_v3)
 
-exportar_html(G, pr_v1, pr_v2, pr_v3, "resultados_tfg.html")
+exportar_html(G, pr_v1, pr_v2, pr_v3, "main.html")
